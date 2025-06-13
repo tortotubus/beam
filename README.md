d7a08d82e52d92faee61f2bcaff532f5ef23abb5
Conor E. Olive <conor@conorolive.com>
2025-06-13 15:45:32 -0700

    add documentation system

---
+++ README.md
@@ -0,0 +1,21 @@
+# Basilisk Beam Library
+
+## Overview
+This C++ library provides tools for beam mechanics calculations, including:
+- Matrix and Vector operations
+- Euler beam solvers
+- Finite element implementations
+
+## Building
+```bash
+mkdir build && cd build
+cmake ..
+make
+```
+
+## Documentation
+Generate documentation using:
+```bash
+make doc
+```
+Then open `docs/html/index.html` in your browser.
--- include/beam/MyClass.hpp
+++ include/beam/MyClass.hpp
@@ -3,16 +3,48 @@
 
 namespace beam {
 
+/**
+ * @brief A simple demonstration class
+ * 
+ * MyClass provides a basic example of class documentation and functionality.
+ * It stores a base value and can perform computations using this value.
+ * 
+ *   The distance between \f$(x_1,y_1)\f$ and \f$(x_2,y_2)\f$ is 
+ * \f(\sqrt{(x_2-x_1)^2+(y_2-y_1)^2}\f)
+ * 
+ *   \f[
+    |I_2|=\left| \int_{0}^T \psi(t) 
+             \left\{ 
+                u(a,t)-
+                \int_{\gamma(t)}^a 
+                \frac{d\theta}{k(\theta,t)}
+                \int_{a}^\theta c(\xi)u_t(\xi,t)\,d\xi
+             \right\} dt
+          \right|
+  \f].
+ */
 class MyClass {
 public:
-  explicit MyClass(int init_value);
-  ~MyClass();
+    /**
+     * @brief Construct a new MyClass object
+     * @param init_value The initial base value to store
+     */
+    explicit MyClass(int init_value);
+    
+    /**
+     * @brief Destroy the MyClass object
+     */
+    ~MyClass();
 
-  // do some work and return a result
-  int compute(int x) const;
+    /**
+     * @brief Compute a result based on input and stored base value
+     * @param x The input value to process
+     * @return int The computed result (base + x²)
+     */
+    int compute(int x) const;
 
 private:
-  int _base;
+    int _base;  ///< The stored base value
 };
 
 } // namespace beam
